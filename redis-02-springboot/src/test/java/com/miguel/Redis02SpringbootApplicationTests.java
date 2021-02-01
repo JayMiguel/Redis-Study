29f70ce5ba7e8ef5d4bd8a4d58041ad9d959caca
zx_panjiaming <595156223@qq.com>
2021-02-01 14:32:56 +0800

    redis-02-springboot: save object

--- redis-02-springboot/src/test/java/com/miguel/Redis02SpringbootApplicationTests.java
+++ redis-02-springboot/src/test/java/com/miguel/Redis02SpringbootApplicationTests.java
@@ -18,19 +18,25 @@ class Redis02SpringbootApplicationTests {
     private RedisTemplate redisTemplate;
 
     @Test
-    void contextLoads() {
-        // 五大数据类型
-        redisTemplate.opsForValue().set("name", "miguel");
-        redisTemplate.opsForHash().put("myhash", "age", 10);
-        redisTemplate.opsForList().leftPush("mylist", "redis");
-        redisTemplate.opsForSet().add("myset", "java");
-        redisTemplate.opsForZSet().add("myzset", "zhangsan", 100);
-        // 三种特殊类型
-        redisTemplate.opsForGeo().add("mygeo", new GeoLocation("Beijing", new Point(116.23, 40.22)));
-        redisTemplate.opsForHyperLogLog().add("mylog", "redis", "mongodb", "mysql");
-        redisTemplate.opsForValue().setBit("mybitmap", 0, false); //opsForValue包含操作String和Bitmap的方法
-        // 获取连接对象
-        RedisConnection connection = redisTemplate.getConnectionFactory().getConnection();
+    void contextLoads() throws JsonProcessingException {
+        // // 五大数据类型
+        // redisTemplate.opsForValue().set("name", "miguel");
+        // redisTemplate.opsForHash().put("myhash", "age", 10);
+        // redisTemplate.opsForList().leftPush("mylist", "redis");
+        // redisTemplate.opsForSet().add("myset", "java");
+        // redisTemplate.opsForZSet().add("myzset", "zhangsan", 100);
+        // // 三种特殊类型
+        // redisTemplate.opsForGeo().add("mygeo", new GeoLocation("Beijing", new Point(116.23, 40.22)));
+        // redisTemplate.opsForHyperLogLog().add("mylog", "redis", "mongodb", "mysql");
+        // redisTemplate.opsForValue().setBit("mybitmap", 0, false); //opsForValue包含操作String和Bitmap的方法
+        // // 获取连接对象
+        // RedisConnection connection = redisTemplate.getConnectionFactory().getConnection();
+
+        // 关于对象的保存
+        User user = new User("miguel", 10);
+        String jsonUser = new ObjectMapper().writeValueAsString(user);
+        redisTemplate.opsForValue().set("user", jsonUser);
+        System.out.println(redisTemplate.opsForValue().get("user"));
     }
 
 }
